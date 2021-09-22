679ef9f4af27a2486ce12de770bc246cbf37303a
Chexpeare <cguevara@nj.rr.com>
2021-09-22 16:19:24 -0400

    Updated: VendingMachineCLI.java

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,13 +1,20 @@
 package com.techelevator;
 
 import com.techelevator.view.Menu;
-
-import javax.sound.midi.Soundbank;
 import java.io.IOException;
-import java.sql.SQLOutput;
 
+/** VendingMachineCLI.java
+ * 		1. Instantiates a menu object and a vending machine calling that menu object.
+ * 		2. Run method passes menu options to menu.getChoiceFromOptions().
+ * 		3. Menu returns user's choice and this vending machine then instantiates a class
+ * 			and calls the respective methods within that new class:
+ * 			Case 1: Inventory and displayInvetory()
+ * 			Case 2: Purchase with its own instantiated menu object and then run the object
+ * 			Case 3: Exits the program
+ * 			Case 4: (Hidden) Calls SalesReport.java, which generates a report
+ */
 public class VendingMachineCLI {
-
+	/** PROPERTIES */
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
@@ -15,13 +22,11 @@ public class VendingMachineCLI {
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE,
 														MAIN_MENU_OPTION_EXIT, MAIN_MENU_OPTION_SALES_REPORT };
 	private final Menu menu;
-//	private final String inputDataFile = "/capstone/vendingmachine.csv";
 
 	/** CONSTRUCTOR */
 	public VendingMachineCLI(Menu menu) {
 		this.menu = menu;
 	}
-
 	public static void main(String[] args) throws IOException {
 		Menu menu = new Menu(System.in, System.out);
 		VendingMachineCLI cli = new VendingMachineCLI(menu);
