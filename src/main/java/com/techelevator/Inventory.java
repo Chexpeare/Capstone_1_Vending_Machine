3016c521e1a791780db4a5a0f08adb21586099eb
Chexpeare <cguevara@nj.rr.com>
2021-09-22 16:16:06 -0400

    Updated: Changed splitUp to splitItemLine for clarity

--- src/main/java/com/techelevator/Inventory.java
+++ src/main/java/com/techelevator/Inventory.java
@@ -1,21 +1,17 @@
 package com.techelevator;
 
 import java.io.*;
-//import java.io.BufferedReader;
-//import java.io.File;
-//import java.io.FileReader;
-//import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 
 public class Inventory {
     /** PROPERTIES */
     private final Map<String, String> itemTypeMap = new HashMap<>();
     private final Map<String, String> itemSelectedMap = new HashMap<>();
-    private String inventoryFile = "/Users/chexpeare/MeritAmerica/PairProgrammingBackup/capstone/vendingmachine.csv";
+    private final String inventoryFile = "/Users/chexpeare/MeritAmerica/PairProgrammingBackup/capstone/vendingmachine.csv";
+//	private final String inputDataFile = "/capstone/vendingmachine.csv";
 
-    /** CONSTRUCTORS */
+    /** CONSTRUCTOR */
     public Inventory() {
 
     }
@@ -23,72 +19,74 @@ public class Inventory {
     /** METHODS */
     public void displayInventory() throws IOException {
         File snackFile = new File(inventoryFile);
-        BufferedReader reader = new BufferedReader(new FileReader(snackFile));
+        FileReader inputFile = new FileReader(snackFile);
+        BufferedReader reader = new BufferedReader(inputFile);
         String currentLine = reader.readLine();
 
         while (currentLine != null) {
-            String[] splitUp = currentLine.split("\\|");
-            System.out.print(splitUp[0] + "  " +  splitUp[1] + "  $" + splitUp[2]);
+            String[] splitItemLine = currentLine.split("\\|");
+            System.out.print(splitItemLine[0] + "  " +  splitItemLine[1] + "  $" + splitItemLine[2]);
 
-            /** INVENTORY: CANDY */
-            if (splitUp[3].equals("Candy")) {
-                Candy candy = new Candy();
-
-                if (splitUp[1].equals("Moonpie")) {
-                    System.out.println("  " +candy.getMoonpiesLeft());
-                } else if (splitUp[1].equals("Cowtales")) {
-                    System.out.println("  " +candy.getCowtalesLeft());
-                } else if (splitUp[1].equals("Wonka Bar")) {
-                    System.out.println("  " +candy.getWonkaBarsLeft());
-                } else if (splitUp[1].equals("Crunchie")) {
-                    System.out.println("  " +candy.getCrunchiesLeft());
-                }
-            }
-
-            /** INVENTORY: CHIPS */
-            if(splitUp[3].equals("Chip")) {
+//          INVENTORY: CHIPS
+            if(splitItemLine[3].equals("Chip")) {
                 Chips chips = new Chips();
 
-                if(splitUp[1].equals("Potato Crisps")) {
+                if(splitItemLine[1].equals("Potato Crisps")) {
                     System.out.println("  " + chips.getPotatoCrispsLeft());
-                }else if(splitUp[1].equals("Stackers")) {
+                }else if(splitItemLine[1].equals("Stackers")) {
                     System.out.println("  " +chips.getStackersLeft());
-                }else if(splitUp[1].equals("Grain Waves")) {
+                }else if(splitItemLine[1].equals("Grain Waves")) {
                     System.out.println("  " +chips.getGrainWavesLeft());
-                }else if(splitUp[1].equals("Cloud Popcorn")) {
+                }else if(splitItemLine[1].equals("Cloud Popcorn")) {
                     System.out.println("  " +chips.getCloudPopcornLeft());
                 }
             }
 
-            /** INVENTORY: GUM */
-            if(splitUp[3].equals("Gum")) {
+//          INVENTORY: CANDY
+            if (splitItemLine[3].equals("Candy")) {
+                Candy candy = new Candy();
+
+                if (splitItemLine[1].equals("Moonpie")) {
+                    System.out.println("  " +candy.getMoonpiesLeft());
+                } else if (splitItemLine[1].equals("Cowtales")) {
+                    System.out.println("  " +candy.getCowtalesLeft());
+                } else if (splitItemLine[1].equals("Wonka Bar")) {
+                    System.out.println("  " +candy.getWonkaBarsLeft());
+                } else if (splitItemLine[1].equals("Crunchie")) {
+                    System.out.println("  " +candy.getCrunchiesLeft());
+                }
+            }
+
+//          INVENTORY: DRINKS
+            if(splitItemLine[3].equals("Drink")) {
+                Drinks drinks = new Drinks();
+
+                if(splitItemLine[1].equals("Cola")) {
+                    System.out.println("  " + drinks.getColaLeft());
+                }else if(splitItemLine[1].equals("Dr. Salt")) {
+                    System.out.println("  " + drinks.getDrSaltLeft());
+                }else if(splitItemLine[1].equals("Mountain Melter")) {
+                    System.out.println("  " + drinks.getMountainMelterLeft());
+                }else if(splitItemLine[1].equals("Heavy")) {
+                    System.out.println("  " + drinks.getHeavyLeft());
+                }
+            }
+
+//          INVENTORY: GUM
+            if(splitItemLine[3].equals("Gum")) {
                 Gum gum = new Gum();
 
-                if(splitUp[1].equals("U-Chews")) {
+                if(splitItemLine[1].equals("U-Chews")) {
                     System.out.println("  " + gum.getuChewsLeft());
-                }else if(splitUp[1].equals("Little League Chew")) {
+                }else if(splitItemLine[1].equals("Little League Chew")) {
                     System.out.println("  " + gum.getLittleLeaugeChewLeft());
-                }else if(splitUp[1].equals("Chiclets")) {
+                }else if(splitItemLine[1].equals("Chiclets")) {
                     System.out.println("  " + gum.getChicletsLeft());
-                }else if(splitUp[1].equals("Triplemint")) {
+                }else if(splitItemLine[1].equals("Triplemint")) {
                     System.out.println("  " + gum.getTriplemintLeft());
                 }
             }
 
-            /** INVENTORY: DRINKS */
-            if(splitUp[3].equals("Drink")) {
-                Drinks drinks = new Drinks();
-
-                if(splitUp[1].equals("Cola")) {
-                    System.out.println("  " + drinks.getColaLeft());
-                }else if(splitUp[1].equals("Dr. Salt")) {
-                    System.out.println("  " + drinks.getDrSaltLeft());
-                }else if(splitUp[1].equals("Mountain Melter")) {
-                    System.out.println("  " + drinks.getMountainMelterLeft());
-                }else if(splitUp[1].equals("Heavy")) {
-                    System.out.println("  " + drinks.getHeavyLeft());
-                }
-            }
             currentLine = reader.readLine();
         }
     }
@@ -99,9 +97,9 @@ public class Inventory {
         String currentLine = reader.readLine();
 
         while(currentLine != null) {
-            String[] splitUp = currentLine.split("\\|");
-            itemSelectedMap.put(splitUp[0], splitUp[1]);
-            itemTypeMap.put(splitUp[0], splitUp[3]);
+            String[] splitItemLine = currentLine.split("\\|");
+            itemSelectedMap.put(splitItemLine[0], splitItemLine[1]);
+            itemTypeMap.put(splitItemLine[0], splitItemLine[3]);
             currentLine = reader.readLine();
         }
     }
