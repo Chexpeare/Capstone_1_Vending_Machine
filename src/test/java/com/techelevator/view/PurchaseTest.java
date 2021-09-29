6934dcb6804edb5e0e55aebd9e1806fa3e3e0549
Chexpeare <cguevara@nj.rr.com>
2021-09-29 17:22:45 -0400

    Update: reposting log.txt and SalesReport.txt

--- src/test/java/com/techelevator/view/PurchaseTest.java
+++ src/test/java/com/techelevator/view/PurchaseTest.java
@@ -1,4 +1,18 @@
 package com.techelevator.view;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PurchaseTest {
+
+    private ByteArrayOutputStream output;
+
+
 }
