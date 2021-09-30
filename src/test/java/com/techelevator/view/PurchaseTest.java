41aa922e02243b6112beaf553ae8fafacbb1fcb4
Chexpeare <cguevara@nj.rr.com>
2021-09-29 23:16:39 -0400

    Developing PurchaseTest.java currently working on makeChangeTest()

--- src/test/java/com/techelevator/view/PurchaseTest.java
+++ src/test/java/com/techelevator/view/PurchaseTest.java
@@ -2,7 +2,10 @@ package com.techelevator.view;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
+import com.techelevator.Inventory;
+import com.techelevator.Purchase;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,13 +19,37 @@ public class PurchaseTest {
 
     @Before
     public void setup() {
-        output = new ByteArrayOutputStream();
+//        this.calc= new Math();
     }
 
     @Test
-    public void test() {
+    public void makeChangeTest() throws IOException {
+        Menu menu = new Menu(System.in, System.out);
+        Inventory inventory = new Inventory();
+        Purchase purchase = new Purchase(menu);
 
+//        Object[] options = new Object[] { 2, 1, 100, 2, "C3"};
+        getMenuForTestingWithUserInput(2 + System.lineSeparator());
+        getMenuForTestingWithUserInput(1 + System.lineSeparator());
+        getMenuForTestingWithUserInput(100 + System.lineSeparator());
+        getMenuForTestingWithUserInput(2 + System.lineSeparator());
+        getMenuForTestingWithUserInput("C3" + System.lineSeparator());
+
+        purchase.feedMoney();
+        purchase.finishTransaction();
+
+        Assert.assertEquals(30 , purchase.getQuarters());
+        Assert.assertEquals(3 , purchase.getDimes());
+        Assert.assertEquals(1 , purchase.getNickels());
     }
 
+    private Menu getMenuForTestingWithUserInput(String userInput) {
+        ByteArrayInputStream input = new ByteArrayInputStream(String.valueOf(userInput).getBytes());
+        return new Menu(input, output);
+    }
+
+    private Menu getMenuForTesting() {
+        return getMenuForTestingWithUserInput("1" + System.lineSeparator());
+    }
 
 }
