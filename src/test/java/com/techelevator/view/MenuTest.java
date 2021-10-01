df99f69ce5ab7220add4b386e26bddc5c9382ad3
Chexpeare <cguevara@nj.rr.com>
2021-10-01 03:29:28 -0400

    Cleaned up code

--- src/test/java/com/techelevator/view/MenuTest.java
+++ src/test/java/com/techelevator/view/MenuTest.java
@@ -8,7 +8,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
-//import com.techelevator.view.Menu;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MenuTest {
